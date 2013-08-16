4d675584fc1466e88860330bbf9922b5174c8f44
meekrabR6R <nmiano84@gmail.com>
2013-08-16 02:26:29 -0400

    Minor formatting adjustments

--- spec/line_maker_spec.rb
+++ spec/line_maker_spec.rb
@@ -27,19 +27,20 @@ describe LineMaker do
 
       second_one.should eq ["0", "0", "0", "0", "1", "1", "1", "0", "0", "0", "0"]
     end
-      #checks 3rd row with an array of length 11
-      it 'should equal ["0", "0", "0", "1", "1", "0", "0", "1", "0", "0", "0"]' do
-      	first_one = @rule.first_line(11)
-	second_one = @rule.next_line(first_one)
-	third_one = @rule.next_line(second_one)
+    
+    #checks 3rd row with an array of length 11
+    it 'should equal ["0", "0", "0", "1", "1", "0", "0", "1", "0", "0", "0"]' do
+      first_one = @rule.first_line(11)
+	    second_one = @rule.next_line(first_one)
+	    third_one = @rule.next_line(second_one)
 
-	third_one.should eq ["0", "0", "0", "1", "1", "0", "0", "1", "0", "0", "0"]
-      end
+	    third_one.should eq ["0", "0", "0", "1", "1", "0", "0", "1", "0", "0", "0"]
+    end
 
-      #checks final row with an array of length 11
-      it 'last line of array of length 11 should equal ["1", "1", "0", "1", "1", "1", "1", "0", "1", "1", "1"]' do
-        last_line = @rule.next_line(["0", "1", "1", "0", "0", "1", "0", "0", "0", "1", "0"])
-      end
+    #checks final row with an array of length 11
+    it 'last line of array of length 11 should equal ["1", "1", "0", "1", "1", "1", "1", "0", "1", "1", "1"]' do
+      last_line = @rule.next_line(["0", "1", "1", "0", "0", "1", "0", "0", "0", "1", "0"])
+    end
   end
 
 end
