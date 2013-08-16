8ec605e54360509452e8fb56813ddac178fb1c5e
meekrabR6R <nmiano84@gmail.com>
2013-08-16 02:30:45 -0400

    Minor formatting adjustments

--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -15,25 +15,25 @@ describe 'Routes' do
   #makes sure index loads correctly
   it "should load the home page" do
     get '/'
-	  last_response.should be_ok
-	end
+      last_response.should be_ok
+    end
 
   #checks various warnings
   it "should return a warning" do
     post '/', params = { array_length: 'XYZ'}
-	  last_response.body.should include("Only Numbers Please!")
+    last_response.body.should include("Only Numbers Please!")
 
-	  post '/', params = { array_length: '2'}
-	  last_response.body.should include("Length Must be >= 3 And <= 75!")
+    post '/', params = { array_length: '2'}
+    last_response.body.should include("Length Must be >= 3 And <= 75!")
 
-	  post '/', params = { array_length: '76'}
-	  last_response.body.should include("Length Must be >= 3 And <= 75!")
+    post '/', params = { array_length: '76'}
+    last_response.body.should include("Length Must be >= 3 And <= 75!")
   end
 
   #makes sure correct next line is sent back to client
   it "should return the next line" do
     post '/getline', params = { last_line: '["0","0","0","1","0","0","0"]'}
-	  last_response.body.should eq "0011100"
+    last_response.body.should eq "0011100"
   end
 
 end
