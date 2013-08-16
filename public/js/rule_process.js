8cec76ef80eebf6c4a0f8d20000f37486c6d83e9
meekrabR6R <nmiano84@gmail.com>
2013-08-16 02:40:47 -0400

    Minor formatting adjustments

--- public/js/rule_process.js
+++ public/js/rule_process.js
@@ -9,121 +9,121 @@
 ***************************************************************/
 var RuleThirty = {
 
-	//variables
-	rowCount : 0,
-	line : [],
-	timerID : 0,
-	stopLoop : 0,
+     //variables
+     rowCount : 0,
+     line : [],
+     timerID : 0,
+     stopLoop : 0,
 
-	/*****************************************************
-	* Begins a loop which sends current line data 
-	* to server and retrieves calculated next line.
-	******************************************************/
-	start : function() {
+     /*****************************************************
+     * Begins a loop which sends current line data 
+     * to server and retrieves calculated next line.
+     ******************************************************/
+     start : function() {
 
-		if(RuleThirty.stopLoop === 1) {
-			RuleThirty.line = [];
-			$('.row'+RuleThirty.rowCount).children('.cell').each(function(){ RuleThirty.line.push(this.id);});
-		}
+          if(RuleThirty.stopLoop === 1) {
+	       RuleThirty.line = [];
+	       $('.row'+RuleThirty.rowCount).children('.cell').each(function(){ RuleThirty.line.push(this.id);});
+	  }
 
-		RuleThirty.stopLoop = 0;
-		$('#start').attr("disabled", true);
-		RuleThirty.getNewLine(RuleThirty.line);
-	},
+	  RuleThirty.stopLoop = 0;
+	  $('#start').attr("disabled", true);
+	  RuleThirty.getNewLine(RuleThirty.line);
+     },
 
-	/*****************************************************
-	* Stops execution of the loop.
-	******************************************************/
-	stop : function() {
+     /*****************************************************
+     * Stops execution of the loop.
+     ******************************************************/
+     stop : function() {
 
-		RuleThirty.stopLoop = 1;
-		$('#start').removeAttr('disabled');
-		RuleThirty.reset();
-	},
+          RuleThirty.stopLoop = 1;
+	  $('#start').removeAttr('disabled');
+	  RuleThirty.reset();
+     },
 
-	/*******************************************************
-	* Resets loop and clears all calculated rows,
-	* leaving only the first row.
-	*********************************************************/
-	reset : function() {
+     /*******************************************************
+     * Resets loop and clears all calculated rows,
+     * leaving only the first row.
+     *********************************************************/
+     reset : function() {
 
-		$('#reset').css('visibility', 'visible');
-		$('#reset').click(function(){
-			$('#iterations').html('');
-			$('#start').css('visibility', 'visible');
-			RuleThirty.rowCount = 0;
-			RuleThirty.line = [];
+          $('#reset').css('visibility', 'visible');
+	  $('#reset').click(function(){
+	       $('#iterations').html('');
+	       $('#start').css('visibility', 'visible');
+	       RuleThirty.rowCount = 0;
+	       RuleThirty.line = [];
+          });
+     },
 
-		});
-	},
+     /*********************************************************
+     * Begins execution of the loop.
+     **********************************************************/
+     getLines : function() {
 
-	/*********************************************************
-	* Begins execution of the loop.
-	**********************************************************/
-	getLines : function() {
+          $('#reset').css('visibility', 'hidden');
+	  $('.row'+RuleThirty.rowCount).children('.cell').each(function(){ RuleThirty.line.push(this.id);});
+	  RuleThirty.start();
+	  $('#stop').click(function(){ RuleThirty.stop();});
 
-		$('#reset').css('visibility', 'hidden');
-		$('.row'+RuleThirty.rowCount).children('.cell').each(function(){ RuleThirty.line.push(this.id);});
-		RuleThirty.start();
-		$('#stop').click(function(){ RuleThirty.stop();});
+     },
 
-	},
+     /***********************************************************
+     * Wraps an ajax post to server with current line info to get
+     * next line.
+     * @param: line - current line
+     ************************************************************/
+     getNewLine : function(line) {
+          $.ajax({
+	       url: '/getline',
+	       type: 'post',
+	       data: {last_line: JSON.stringify(line)},
+	       async: true,
+	       success: function(result){ RuleThirty.callBack(result); }
+	  });
+     },
 
-    /***********************************************************
-    * Wraps an ajax post to server with current line info to get
-    * next line.
-    * @param: line - current line
-    ************************************************************/
-	getNewLine : function(line) {
-		$.ajax({
-			url: '/getline',
-			type: 'post',
-			data: {last_line: JSON.stringify(line)},
-			async: true,
-			success: function(result){ RuleThirty.callBack(result); }
-		});
-	},
+     /***********************************************************
+     * Callback function for post to /getline. Handles the data
+     * returned by the server. Exported out of the jQuery $.post
+     * primarily for readability.
+     * @param : result - cellular on/off statuses for next line
+     ************************************************************/
+     callBack : function(result) {
+          RuleThirty.rowCount++;
+	  $('#iterations').append('<br><div class="row row'+RuleThirty.rowCount+'"></div>');
 
-	/***********************************************************
-	* Callback function for post to /getline. Handles the data
-	* returned by the server. Exported out of the jQuery $.post
-	* primarily for readability.
-	* @param : result - cellular on/off statuses for next line
-	************************************************************/
-	callBack : function(result) {
-		RuleThirty.rowCount++;
-		$('#iterations').append('<br><div class="row row'+RuleThirty.rowCount+'"></div>');
+          var id = "";
+	  var stopCheck = 0;
+	  RuleThirty.line = [];
 
-		var id = "";
-		var stopCheck = 0;
-		RuleThirty.line = [];
+          for(i=0; i<result.length; i++){
 
-		for(i=0; i<result.length; i++){
+               if((i === 0 || i === result.length-1) && result[i] === '1')
+	            stopCheck = 1;
 
-			if((i === 0 || i === result.length-1) && result[i] === '1')
-				stopCheck = 1;
+	       RuleThirty.line[i] = result[i];
 
-			RuleThirty.line[i] = result[i];
+	       if(result[i] === '1') 
+	            id = "1";
+	       else 
+	            id="0";
 
-			if(result[i] === '1') id = "1";
-			else id="0";
+	       $('.row'+RuleThirty.rowCount).append('<div class="cell cell'+id+'" id="'+id+'"></div>');
+          }
 
-			$('.row'+RuleThirty.rowCount).append('<div class="cell cell'+id+'" id="'+id+'"></div>');
-		}
-
-
-        	if(stopCheck === 1){
-			$('#start').css('visibility', 'hidden');
-			RuleThirty.stop();
-		}
-		else if(RuleThirty.stopLoop == 1)
-			RuleThirty.reset();
-		else
-			RuleThirty.getNewLine(RuleThirty.line);
-	}
+          if(stopCheck === 1){
+	       $('#start').css('visibility', 'hidden');
+	       RuleThirty.stop();
+	  }
+	  else if(RuleThirty.stopLoop == 1)
+	       RuleThirty.reset();
+	  else
+	       RuleThirty.getNewLine(RuleThirty.line);
+     }
 };
 
 //begins execution
 $(document).ready(function() {
-	$("#start").click(function() { RuleThirty.getLines(); });
+     $("#start").click(function() { RuleThirty.getLines(); });
 });
